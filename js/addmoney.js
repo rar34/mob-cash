f611b0e196fc1b9e4a04b4aca15d9c3f90aa5332
rar34 <ra.cse34@gmail.com>
2025-08-16 13:00:53 +0600

    add and event and make add money form visible when clicked in add money button

--- js/addmoney.js
+++ js/addmoney.js
@@ -1,3 +1,5 @@
 document.getElementById('add-money').addEventListener('click', function(){
-    
+    const addMoneyForm = document.getElementById('add-money-form');
+    addMoneyForm.classList.remove('hidden');
+    // console.log('btn clicked')
 })
