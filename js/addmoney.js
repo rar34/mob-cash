945e2e9c95e0740ed99f840595d84f920dffd0d7
rar34 <ra.cse34@gmail.com>
2025-08-16 17:01:58 +0600

    added functionality to add money and show total money to the top of application

--- js/addmoney.js
+++ js/addmoney.js
@@ -1,5 +1,24 @@
 document.getElementById('add-money').addEventListener('click', function(){
     const addMoneyForm = document.getElementById('add-money-form');
     addMoneyForm.classList.remove('hidden');
-    // console.log('btn clicked')
+   
+})
+
+document.getElementById('btn-add-money').addEventListener('click', function(e){
+    e.preventDefault();
+    const amountText = document.getElementById('amount').value;
+    const amount = parseInt(amountText);
+
+    const pin = document.getElementById('pin-number').value;
+
+    const totalAmountElement = document.getElementById('total-amount');
+    let totalAmount = parseInt(totalAmountElement.innerText);
+
+    if(pin === '1234'){
+        totalAmount = totalAmount + amount;
+        totalAmountElement.innerText = totalAmount;
+    }
+    else{
+        alert('wrong pin')
+    }
 })
