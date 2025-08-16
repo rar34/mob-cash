0755975cd4607fadefaae46e0a50030314368cdc
rar34 <ra.cse34@gmail.com>
2025-08-16 12:14:35 +0600

    implemented logout functionality

--- js/login.js
+++ js/login.js
@@ -1,14 +1,14 @@
-document.getElementById('btn-login').addEventListener('click', function(e){
+document.getElementById('btn-login').addEventListener('click', function (e) {
     e.preventDefault();
     // console.log('btn clicked')
 
     const phoneNumber = document.getElementById('phone-number').value;
     const pinNumber = document.getElementById('pin-number').value;
 
-    if(phoneNumber === '01745' && pinNumber === '1234'){
+    if (phoneNumber === '01745' && pinNumber === '1234') {
         window.location.href = "/home.html";
     }
-    else{
+    else {
         alert("wrong credentials")
     }
 })
