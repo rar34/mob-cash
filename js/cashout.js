366067f54dff5ff1b82f8f15b4e7ff59585e60bb
rar34 <ra.cse34@gmail.com>
2025-08-16 17:47:22 +0600

    added cashout functionality and successfully deduct money

--- js/cashout.js
+++ js/cashout.js
@@ -6,18 +6,18 @@ document.getElementById('cashout').addEventListener('click', function(){
    
 })
 
-document.getElementById('btn-add-money').addEventListener('click', function(e){
+document.getElementById('btn-cashout').addEventListener('click', function(e){
     e.preventDefault();
-    const amountText = document.getElementById('amount').value;
+    const amountText = document.getElementById('amount-cash').value;
     const amount = parseInt(amountText);
 
-    const pin = document.getElementById('pin-number').value;
+    const pin = document.getElementById('pin-number-cash').value;
 
     const totalAmountElement = document.getElementById('total-amount');
     let totalAmount = parseInt(totalAmountElement.innerText);
 
     if(pin === '1234'){
-        totalAmount = totalAmount + amount;
+        totalAmount = totalAmount - amount;
         totalAmountElement.innerText = totalAmount;
     }
     else{
