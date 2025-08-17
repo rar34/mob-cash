0505acaee09af28a5abc6d5603a94b5a4d56d02e
rar34 <ra.cse34@gmail.com>
2025-08-17 22:46:26 +0600

    added transaction history section

--- js/addmoney.js
+++ js/addmoney.js
@@ -3,7 +3,14 @@ document.getElementById('add-money').addEventListener('click', function(){
     const cashoutForm = document.getElementById('cashout-form');
     addMoneyForm.classList.remove('hidden');
     cashoutForm.classList.add('hidden')
-   
+    document.getElementById('transaction-container').classList.add('hidden')
+})
+document.getElementById('transaction').addEventListener('click', function(){
+    const addMoneyForm = document.getElementById('add-money-form');
+    const cashoutForm = document.getElementById('cashout-form');
+    addMoneyForm.classList.add('hidden');
+    cashoutForm.classList.add('hidden')
+    document.getElementById('transaction-container').classList.remove('hidden')
 })
 
 document.getElementById('btn-add-money').addEventListener('click', function(e){
@@ -19,6 +26,8 @@ document.getElementById('btn-add-money').addEventListener('click', function(e){
     if(pin === '1234'){
         totalAmount = totalAmount + amount;
         totalAmountElement.innerText = totalAmount;
+
+        const p = document.createElement('p')
     }
     else{
         alert('wrong pin')
